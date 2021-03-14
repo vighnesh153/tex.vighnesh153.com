365d47128cec4d98bafce363f32fed99c7acd5ca
Vighnesh Raut <vighnesh.raut13@gmail.com>
2021-03-14 12:40:47 +0530

    add download functionality

--- src/EditorAndPreview.js
+++ src/EditorAndPreview.js
@@ -1,13 +1,17 @@
 import React from "react";
+// import axios from "axios";
 
 import {useTheme} from "@material-ui/core";
 
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
 import MathComponent from "./MathComponent";
+
+import html2canvas from "html2canvas";
 
 import Store from "./store";
 
@@ -36,6 +40,36 @@ function EditorAndPreview() {
     };
   }, [editorValue]);
 
+  const download = () => {
+    html2canvas(document.querySelector("#preview-content")).then(canvas => {
+      const imageData = canvas.toDataURL("image/png");
+      console.log(canvas.toDataURL());
+      const link = document.createElement('a');
+      link.download = 'image.png';
+      link.href = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+      link.click();
+    });
+  };
+
+  // const upload = () => {
+  //   html2canvas(document.querySelector("#preview-content")).then(canvas => {
+  //     const imageData = canvas.toDataURL().split(',')[1];
+  //     axios.post("https://api.imgur.com/3/upload", {
+  //       image: imageData,
+  //     }, {
+  //       headers: {
+  //         Authorization: `Client-ID cd249eb8713e884`,
+  //       }
+  //     })
+  //       .then((res) => {
+  //         console.log(res);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   });
+  // };
+
   const styles = {
     root: {
       height: '100%',
@@ -50,8 +84,8 @@ function EditorAndPreview() {
     sectionTitle: {
       width: '100%',
       maxWidth: 'unset',
-      // position: 'relative',
       paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
       border: `1px solid ${theme.palette.divider}`,
       borderLeft: 'none',
       borderRight: 'none',
@@ -91,16 +125,17 @@ function EditorAndPreview() {
       <Box style={styles.section}>
         <Grid
           item container xs={1} alignItems={"center"}
-          justify={"flex-start"} style={styles.sectionTitle}
+          justify={"space-between"} style={styles.sectionTitle}
         >
           <Typography variant={"h6"} style={{fontWeight: 'bold'}}>
             Preview
           </Typography>
+          <Button onClick={download} variant={"outlined"}>
+            Download
+          </Button>
         </Grid>
         <Grid item xs={11} style={styles.sectionContent}>
-          <Box
-            padding={2}
-          >
+          <Box padding={2} id="preview-content">
             <MathComponent display tex={editorValue} />
           </Box>
         </Grid>
